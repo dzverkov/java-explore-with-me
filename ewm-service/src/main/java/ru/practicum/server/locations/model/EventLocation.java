1eb66d53ec45284fe3c3cd8a12a3a62e7c93521b
Dmitry Zverkov <d.zverkov@yandex.ru>
2023-01-14 21:58:02 +0300

    Init

---
+++ ewm-service/src/main/java/ru/practicum/server/locations/model/EventLocation.java
@@ -0,0 +1,16 @@
+package ru.practicum.server.locations.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "events", schema = "public")
+public class EventLocation {
+}
